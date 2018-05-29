a1f292e6416a40492b2d093ab270461cdd3012fd
Narayan Ambatipudi <nambatipudi@brandywineglobal.com>
2018-05-29 16:21:38 -0400

    Utility to convert image files to base64 strings.
    Signed-off-by: Narayan Ambatipudi

---
+++ ImageToBase64/FrmImagetoBase64Main.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ImageToBase64
+{
+    partial class FrmImagetoBase64Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImageFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnOpenFileSelectDialogue = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtBase64String = new System.Windows.Forms.TextBox();
+            this.lblSelectFile = new System.Windows.Forms.Label();
+            this.lblHorizontalLine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openImageFileDialog
+            // 
+            this.openImageFileDialog.Filter = "Image files|*.jpg, *.jpeg, *.jpe, *.jfif, *.png,*.bmp";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(72, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(341, 20);
+            this.txtFilePath.TabIndex = 0;
+            // 
+            // btnOpenFileSelectDialogue
+            // 
+            this.btnOpenFileSelectDialogue.Location = new System.Drawing.Point(419, 12);
+            this.btnOpenFileSelectDialogue.Name = "btnOpenFileSelectDialogue";
+            this.btnOpenFileSelectDialogue.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFileSelectDialogue.TabIndex = 1;
+            this.btnOpenFileSelectDialogue.Text = "Select";
+            this.btnOpenFileSelectDialogue.UseVisualStyleBackColor = true;
+            this.btnOpenFileSelectDialogue.Click += new System.EventHandler(this.btnOpenFileSelectDialogue_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(419, 43);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(338, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // txtBase64String
+            // 
+            this.txtBase64String.Location = new System.Drawing.Point(12, 72);
+            this.txtBase64String.Multiline = true;
+            this.txtBase64String.Name = "txtBase64String";
+            this.txtBase64String.ReadOnly = true;
+            this.txtBase64String.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBase64String.Size = new System.Drawing.Size(482, 173);
+            this.txtBase64String.TabIndex = 4;
+            // 
+            // lblSelectFile
+            // 
+            this.lblSelectFile.AutoSize = true;
+            this.lblSelectFile.Location = new System.Drawing.Point(10, 15);
+            this.lblSelectFile.Name = "lblSelectFile";
+            this.lblSelectFile.Size = new System.Drawing.Size(56, 13);
+            this.lblSelectFile.TabIndex = 6;
+            this.lblSelectFile.Text = "Select File";
+            // 
+            // lblHorizontalLine
+            // 
+            this.lblHorizontalLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHorizontalLine.Location = new System.Drawing.Point(12, 38);
+            this.lblHorizontalLine.MaximumSize = new System.Drawing.Size(0, 2);
+            this.lblHorizontalLine.MinimumSize = new System.Drawing.Size(0, 2);
+            this.lblHorizontalLine.Name = "lblHorizontalLine";
+            this.lblHorizontalLine.Size = new System.Drawing.Size(0, 2);
+            this.lblHorizontalLine.TabIndex = 8;
+            // 
+            // FrmImagetoBase64Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 257);
+            this.Controls.Add(this.lblHorizontalLine);
+            this.Controls.Add(this.lblSelectFile);
+            this.Controls.Add(this.txtBase64String);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnOpenFileSelectDialogue);
+            this.Controls.Add(this.txtFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmImagetoBase64Main";
+            this.Text = "Image to base 64 converter";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmImagetoBase64Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmImagetoBase64Main_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openImageFileDialog;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnOpenFileSelectDialogue;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtBase64String;
+        private System.Windows.Forms.Label lblSelectFile;
+        private System.Windows.Forms.Label lblHorizontalLine;
+    }
+}
+
---
+++ ImageToBase64/FrmImagetoBase64Main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace ImageToBase64
+{
+    public partial class FrmImagetoBase64Main : Form
+    {
+        public FrmImagetoBase64Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFileSelectDialogue_Click(object sender, EventArgs e)
+        {
+            var dialogueResult = openImageFileDialog.ShowDialog();
+            if (dialogueResult == DialogResult.Cancel)
+                return;
+            convert(openImageFileDialog.FileName);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBase64String.Text))
+                return;
+            Clipboard.SetText(txtBase64String.Text);
+        }
+
+        private void convert(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("File :" + fileName + " can not be found.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            try
+            {
+                txtFilePath.Text = fileName;
+                //https://stackoverflow.com/questions/21325661/convert-image-path-to-base64-string
+                using (Image image = Image.FromFile(fileName))
+                {
+                    using (MemoryStream m = new MemoryStream())
+                    {
+                        image.Save(m, image.RawFormat);
+                        byte[] imageBytes = m.ToArray();
+
+                        // Convert byte[] to Base64 String
+                        txtBase64String.Text = Convert.ToBase64String(imageBytes);
+
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error: "+ exp.Message, "Error", MessageBoxButtons.OK);
+            }
+        }
+        private void FrmImagetoBase64Main_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                switch (Path.GetExtension(file).ToUpper())
+                {
+                    //Image files|*.jpg, *.jpeg, *.jpe, *.jfif, *.png,*.bmp
+                    case ".JPEG":
+                    case ".BMP":
+                    case ".PNG":
+                    case ".JFIF":
+                    case ".JPE":
+                    case ".JPG":
+                    {
+                        convert(file);
+                        return;
+                    }
+
+                    default:
+                        continue;
+
+                }
+            }
+        }
+
+        private void FrmImagetoBase64Main_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+    }
+}
